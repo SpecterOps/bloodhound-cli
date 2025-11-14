af8591dadfb66cc6ac39b2b3cc5444b4f93e678e
Christopher Maddalena <chris.maddalena@protonmail.com>
2025-11-13 19:11:09 -0800

    Updated Docker client to latest moby client

--- cmd/internal/docker.go
+++ cmd/internal/docker.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
+	"github.com/moby/moby/api/types/container"
+	"github.com/moby/moby/client"
 )
 
 // Vars for tracking the list of BloodHound images
@@ -36,7 +36,7 @@ type Container struct {
 	ID     string
 	Image  string
 	Status string
-	Ports  []types.Port
+	Ports  []container.PortSummary
 	Name   string
 }
 
@@ -288,19 +288,19 @@ func RunDockerComposePull(yaml string) {
 // FetchLogs fetches logs from the container with the specified "name" label ("containerName" parameter).
 func FetchLogs(containerName string, lines string) []string {
 	var logs []string
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	cli, err := client.New(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		log.Fatalf("Failed to get client in logs: %v", err)
 	}
-	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	containers, err := cli.ContainerList(context.Background(), client.ContainerListOptions{})
 	if err != nil {
 		log.Fatalf("Failed to get container list: %v", err)
 	}
-	if len(containers) > 0 {
-		for _, container := range containers {
+	if len(containers.Items) > 0 {
+		for _, container := range containers.Items {
 			if container.Labels["name"] == containerName || containerName == "all" || container.Labels["name"] == "bhce_"+containerName {
 				logs = append(logs, fmt.Sprintf("\n*** Logs for `%s` ***\n\n", container.Labels["name"]))
-				reader, err := cli.ContainerLogs(context.Background(), container.ID, types.ContainerLogsOptions{
+				reader, err := cli.ContainerLogs(context.Background(), container.ID, client.ContainerLogsOptions{
 					ShowStdout: true,
 					ShowStderr: true,
 					Tail:       lines,
@@ -334,18 +334,18 @@ func FetchLogs(containerName string, lines string) []string {
 func GetRunning() Containers {
 	var running Containers
 
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	cli, err := client.New(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		log.Fatalf("Failed to get client connection to Docker: %v", err)
 	}
-	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{
+	containers, err := cli.ContainerList(context.Background(), client.ContainerListOptions{
 		All: false,
 	})
 	if err != nil {
 		log.Fatalf("Failed to get container list from Docker: %v", err)
 	}
-	if len(containers) > 0 {
-		for _, container := range containers {
+	if len(containers.Items) > 0 {
+		for _, container := range containers.Items {
 			if Contains(devImages, container.Labels["name"]) || Contains(prodImages, container.Labels["name"]) {
 				running = append(running, Container{
 					container.ID, container.Image, container.Status, container.Ports, container.Labels["name"],
